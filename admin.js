5a289a18a9edb42087024fc6606f15cb2e3ca07f
bill41 <willieboy14@outlook.com>
2017-12-06 20:34:13 -0500

    Save courses to local storage as an array of objects

--- admin.js
+++ admin.js
@@ -1,16 +1,13 @@
 "use strict";
 
-var getById = function (id) { return document.getElementById(id); };
+const LOCAL_STORAGE_KEY = "asu-courses";
 
-var names = [];
-var numbers = [];
-var dates = [];
-var lengths = [];
-var days = [];
-var times = [];
-var descriptions = [];
+var getById = function (id) { return document.getElementById(id); };
+var courses = [];
 
 var addClass = function() {
+
+    var course = {};
     var courseName = getById("course-name").value;
     var courseNumber = getById("course-number").value.toUpperCase();
     var courseDate = getById("course-date").value;
@@ -21,26 +18,24 @@ var addClass = function() {
 
     validateInput(courseName, courseNumber, courseDate, courseLength, courseDay, courseTime, courseDesc);
 
-    names.push(courseName);
-    numbers.push(courseNumber);
-    dates.push(courseDate);
-    lengths.push(courseLength);
-    days.push(courseDay);
-    times.push(courseTime);
-    descriptions.push(courseDesc);
+    course = {
+        name: courseName,
+        number: courseNumber,
+        date: courseDate,
+        length: courseLength,
+        day: courseDay,
+        time: courseTime,
+        description: courseDesc
+    };
 
-    addLocalStorage(names, numbers, dates, lengths, days, times, descriptions);
+    courses.push(course);
+
+    addLocalStorage(courses);
 
 };
 
-var addLocalStorage = function(names, numbers, dates, lengths, days, times, descriptions) {
-    localStorage.setItem("names", JSON.stringify(names));
-    localStorage.setItem("numbers", JSON.stringify(numbers));
-    localStorage.setItem("dates", JSON.stringify(dates));
-    localStorage.setItem("lengths", JSON.stringify(lengths));
-    localStorage.setItem("days", JSON.stringify(days));
-    localStorage.setItem("times", JSON.stringify(times));
-    localStorage.setItem("descriptions", JSON.stringify(descriptions));
+var addLocalStorage = function() {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(courses));
 }
 
 var validateInput = function(courseName, courseNumber, courseDate, courseLength, courseDay, courseTime, courseDesc) {
